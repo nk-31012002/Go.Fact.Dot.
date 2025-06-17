2880b0f7230643c95da70ee1f91849bcf38bff23
Nagendra Kumar <nagendrakumar@Nagendras-MacBook-Pro.local>
2025-06-17 16:28:42 +0530

    ++

---
+++ variadic_function.go
@@ -0,0 +1,32 @@
+//package main
+//
+//import "fmt"
+//
+///*
+//	A variadic function is a function that accepts a variable number of arguments of the same type.
+//
+//	syntax ->
+//	func function_name(parameter_name ... parameter_type) return_type {
+//
+//		//body
+//
+//	}
+//*/
+//
+//func solve(nums ...int) int {
+//	total := 0
+//
+//	for _, num := range nums {
+//		total += num
+//	}
+//
+//	return total
+//}
+//
+//func main() {
+//	fmt.Println("Go Start")
+//
+//	fmt.Println(solve(1, 2, 3))
+//
+//	fmt.Println("Go End")
+//}
