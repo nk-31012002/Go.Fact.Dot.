9b61697c7e926af2e2ef1b063894281bf8b5642e
Nagendra Kumar <nagendrakumar@Nagendras-MacBook-Pro.local>
2025-06-19 14:26:04 +0530

    interface info

--- interfaces.go
+++ interfaces.go
@@ -3,9 +3,78 @@ package main
 import "fmt"
 
 /*
-	In system design -> our function should be ***closed for modification but open for extensions***
+	Problem Without interface -> Imagine you're building a program where different animals make different sounds. You want a function to make any animal speak. You need to write a new function every time you add a new animal. You can't easily generalize behavior like "make any animal speak".Your code is repetitive and tightly coupled to concrete types.
+	Inshort: --> ***for efficient function, it should follow -> ```Open for extensions but, close for modification``` ***
+										****code ->         package main
 
-	below is the prob
+														import "fmt"
+
+														type Dog struct{}
+														type Cat struct{}
+
+														func DogSpeak(d Dog) {
+															fmt.Println("Woof!")
+														}
+
+														func CatSpeak(c Cat) {
+															fmt.Println("Meow!")
+														}
+
+														func main() {
+															d := Dog{}
+															c := Cat{}
+
+															DogSpeak(d)
+															CatSpeak(c)
+														}****
+
+
+	Solution With interface -> We define an Animal interface with a Speak() method. Dog and Cat both implement that interface by defining Speak(). The MakeItSpeak() function now works with any type that satisfies the Animal interface.
+									****code ->         package main
+
+														import "fmt"
+
+														// Define an interface
+														type Animal interface {
+															Speak() string
+														}
+
+														// Dog implements the interface
+														type Dog struct{}
+														func (d Dog) Speak() string {
+															return "Woof!"
+														}
+
+														// Cat implements the interface
+														type Cat struct{}
+														func (c Cat) Speak() string {
+															return "Meow!"
+														}
+
+														// General function using the interface
+														func MakeItSpeak(a Animal) {
+															fmt.Println(a.Speak())
+														}
+
+														func main() {
+															d := Dog{}
+															c := Cat{}
+
+															MakeItSpeak(d)
+															MakeItSpeak(c)
+														}
+														****
+Interface Gives :-
+
+Flexibility: Easily add new animal types without changing the function.
+
+Reusability: One function handles all "speaking" behavior.
+
+Loose coupling: Your code depends on behavior (interface), not concrete types.
+
+
+
+
 */
 
 type payments struct {
