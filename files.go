1cfb74725458049a6d8b3e1a6394fd35d360fa46
Nagendra Kumar <nagendrakumar@Nagendras-MacBook-Pro.local>
2025-06-23 13:07:44 +0530

    read files in go

--- files.go
+++ files.go
@@ -6,16 +6,37 @@ import (
 )
 
 func main() {
+	//
+	//f, err := os.Open("example.txt")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//fileInfo, err := f.Stat()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//fmt.Println("file name:", fileInfo.Name())
+
+	//read file
 
 	f, err := os.Open("example.txt")
+
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	buf := make([]byte, 10)
+	d, err := f.Read(buf)
 	if err != nil {
 		panic(err)
 	}
 
-	fileInfo, err := f.Stat()
-	if err != nil {
-		panic(err)
+	for i := 0; i < len(buf); i++ {
+		println("data", d, string(buf[i]))
 	}
 
-	fmt.Println("file name:", fileInfo.Name())
+	fmt.Println("data", d, buf)
 }
